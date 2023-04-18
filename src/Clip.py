bbb7868d5d519d7694120d8718b9182a51317b47
Collin Kees <Collin@kees.net>
2023-04-18 13:51:19 -0700

    add clip title and game_id

--- src/Clip.py
+++ src/Clip.py
@@ -3,17 +3,19 @@
 import datetime
 
 class Clip:
-    def __init__(self, clip_dir, clip_id, streamer_id, streamer_name, time, duration, view_count=None):
+    def __init__(self, clip_dir, clip_id, streamer_id, game_id, streamer_name, time, duration, view_count=None, title=None):
         self.clip_dir = clip_dir
         self.clip_id = clip_id
         self.streamer_id = streamer_id
+        self.game_id = game_id
         self.streamer_name = streamer_name
         self.view_count = view_count
         self.time = time
         self.duration = duration
+        self.title = title
 
     def to_string(self):
-        return f'{self.clip_dir}, {self.clip_id}, {self.streamer_id}, {self.streamer_name}, {self.time}, {self.duration}, {self.view_count}'
+        return f'{self.clip_dir}, {self.clip_id}, {self.streamer_id}, {self.game_id}, {self.streamer_name}, {self.time}, {self.duration}, {self.view_count}, {self.title}'
     
     # a static method that takes in a string and returns a clip object
     @staticmethod
@@ -25,8 +27,8 @@ class Clip:
         split[5] = float(split[5])
         split[6] = split[6].strip('\n')
 
-        return Clip(split[0], split[1], split[2], split[3], split[4], split[5], split[6])
+        return Clip(split[0], split[1], split[2], split[3], split[4], split[5], split[6], split[7], split[8])
     
     @staticmethod
     def from_twitch_api_clip(clip, clip_dir):
-        return Clip(clip_dir, clip['id'], clip['broadcaster_id'], clip['broadcaster_name'], datetime.datetime.fromisoformat(str(clip['created_at'])), clip['duration'], clip['view_count'])
+        return Clip(clip_dir, clip['id'], clip['broadcaster_id'], clip['game_id'], clip['broadcaster_name'], datetime.datetime.fromisoformat(str(clip['created_at'])), clip['duration'], clip['view_count'], clip['title'])
