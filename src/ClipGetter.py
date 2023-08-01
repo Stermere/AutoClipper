89916c80ef307b3dfc342c25ad0aae74fc2edc52
Collin Kees <Collin@kees.net>
2023-08-01 16:06:05 -0700

    allow for clipping even with deleted vod

--- src/ClipGetter.py
+++ src/ClipGetter.py
@@ -2,6 +2,7 @@
 import os
 import requests
 import datetime
+import re
 from datetime import timezone
 from src.ClipCompiler import ClipCompiler
 from src.Clip import Clip
@@ -19,7 +20,7 @@ class ClipGetter:
         # download the clip
         index = clip['thumbnail_url'].find('-preview')
         clip_url = clip['thumbnail_url'][:index] + '.mp4'
-        clip_name = f'{clip_dir}/{user.display_name}_{clip.vod_offset}_{clip.video_id}.mp4'
+        clip_name = f'{clip_dir}/{user.display_name}_{clip.vod_offset}_{clip.id}.mp4'
         r = requests.get(clip_url)
         if r.headers['Content-Type'] == 'binary/octet-stream':
             if not os.path.exists(clip_dir):
@@ -28,6 +29,16 @@ class ClipGetter:
                 f.write(r.content)
 
         return clip_name
+    
+    def parse_duration(self, duration_str):
+        pattern = r'(\d+)h(\d+)m(\d+)s'
+        match = re.match(pattern, duration_str)
+        if match:
+            hours, minutes, seconds = map(int, match.groups())
+            return datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        else:
+            raise ValueError("Invalid duration string format")
+
 
     # get the most popular clips from a streamer and download them. designed to be used when making compilation videos
     def get_clips_from_stream(self, user, client, clip_dir=DEFAULT_SAVE_DIR, clip_count=15, vods_back=0):
@@ -40,28 +51,31 @@ class ClipGetter:
         # sort the videos by creation time
         videos.sort(key=lambda x: x['created_at'], reverse=True)
 
-        # get the video and its id
-        video_id = videos[vods_back]['id']
+        video = videos[vods_back]
+        video_id = video['id']
+        video_duration = self.parse_duration(video['duration'])
+        print(f'Video duration: {video_duration}')
 
         # the start time for getting clips should be the origin of the video
-        start_time = (videos[vods_back]['created_at'].astimezone() - datetime.timedelta(hours=5)).isoformat()
-        
+        start_time = (videos[vods_back]['created_at'].astimezone() - (video_duration + datetime.timedelta(hours=5))).isoformat()
+        start_time_dt = datetime.datetime.fromisoformat(start_time)
+
         # get clips from this user's streams in the time after the stream started
         clips = client.get_clips(user.id, started_at=start_time, page_size=100)
 
         # filter to only include clips that are from the most recent stream
         clips_temp = []
-        i = 0
-        for clip in clips:
-            if video_id == clip['video_id']:
-                clips_temp.append(clip)
-            if i >= clip_count:
-                break
-            i += 1
-        clips = clips_temp
+        for i in range(len(clips)):
+            # if vod offset is none then the video is not up or to recent
+            # so fall back to time based filtering
+            if clips[i]['vod_offset'] == None:
+                if clips[i]['created_at'].astimezone() > start_time_dt:
+                    clips_temp.append(clips[i])
+            else:
+                if video_id == clips[i]['video_id']:
+                    clips_temp.append(clips[i])
 
-        # vod offset should always be available here since the video was still up
-        clips.sort(key=lambda x: x['vod_offset'], reverse=True)
+        clips = clips_temp
 
         # create a folder for the clips
         if not os.path.exists(clip_dir):
@@ -70,7 +84,7 @@ class ClipGetter:
         print(f'Found {min(len(clips), clip_count)} clips for {user.display_name}\'s stream ({video_id}) looking back to {start_time}')
 
         # print how long its been since the stream started
-        print(f'Time since stream ended: {(datetime.datetime.now().astimezone() - datetime.datetime.fromisoformat(start_time).astimezone()).total_seconds() / 60 / 60:.2f} hours')
+        print(f'Time since stream ended: {(datetime.datetime.now().astimezone() - start_time_dt.astimezone()).total_seconds() / 60 / 60:.2f} hours')
 
         if len(clips) == 0:
             return []
